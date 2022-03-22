9337259dc754c447eeece9ca8be584854a27b094
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-03-22 21:45:59 +0100

    move the scene to a yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use raytracer::canvas::ImageCanvas;
-use raytracer::color::Color;
-use raytracer::geometry::Sphere;
-use raytracer::math::Vector3;
 use raytracer::render::render;
 use raytracer::scene::Scene;
+use std::path::Path;
 
 fn main() {
     // Settings
@@ -11,16 +9,8 @@ fn main() {
     let fb_size = (640, 480);
 
     // Create the scene
-    let mut scene = Scene::new();
-
-    let sphere = Sphere::new(Vector3::new(0.0, -1.0, 3.0), 1.0, Color::RED);
-    scene.spheres.push(sphere);
-
-    let sphere = Sphere::new(Vector3::new(2.0, 0.0, 4.0), 1.0, Color::BLUE);
-    scene.spheres.push(sphere);
-
-    let sphere = Sphere::new(Vector3::new(-2.0, 0.0, 4.0), 1.0, Color::GREEN);
-    scene.spheres.push(sphere);
+    let scene_path = Path::new("data/scenes/scene1.yml");
+    let scene = Scene::load_from_file(&scene_path);
 
     // Render the scene
     let mut canvas = ImageCanvas::new(fb_size.0, fb_size.1);
