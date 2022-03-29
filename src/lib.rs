0c493a6cf2e49439e7c98700bbce3362689c73b5
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-03-29 19:34:46 +0200

    can output canvas as image instead of RT render

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod canvas;
 pub mod color;
 pub mod geometry;
+pub mod image;
 pub mod math;
 pub mod render;
 pub mod scene;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 
 use raytracer::canvas::FrameBufferCanvas;
+use raytracer::image::save_canvas_to_file;
 use raytracer::render::render;
 use raytracer::scene::Scene;
 use raytracer::sdl::copy_canvas_to_texture;
@@ -18,6 +19,7 @@ pub fn main() -> Result<(), String> {
     let mut canvas_width = 640;
     let mut canvas_height = 480;
     let mut scene_path = Path::new("");
+    let mut output_image_path = None;
     let args: Vec<String> = env::args().collect();
     let args_count = args.len();
     let mut i = 1;
@@ -35,6 +37,11 @@ pub fn main() -> Result<(), String> {
                 canvas_height = next_arg.parse().unwrap();
                 i += 1;
             }
+        } else if arg == "-o" || arg == "--output" {
+            if let Some(next_arg) = next_arg {
+                output_image_path = Some(Path::new(next_arg));
+                i += 1;
+            }
         } else {
             scene_path = Path::new(arg);
         }
@@ -47,59 +54,82 @@ pub fn main() -> Result<(), String> {
 
     // Create the scene
     let scene = Scene::load_from_file(&scene_path);
-    // Inialize the sdl
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem
-        .window("Raytracer", canvas_width, canvas_height)
-        .position_centered()
-        .opengl()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let mut sdl_canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-    let texture_creator = sdl_canvas.texture_creator();
-    let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, canvas_width, canvas_height)
-        .map_err(|e| e.to_string())?;
-
-    let mut event_pump = sdl_context.event_pump()?;
+    // Create the canvas
     let mut canvas = FrameBufferCanvas::new(canvas_width, canvas_height);
 
-    let mut previous_now = Instant::now();
-    let mut frame_count = 1;
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-
-        sdl_canvas.set_draw_color(Color::RGB(255, 0, 255));
-        sdl_canvas.clear();
-
+    // SDL related code
+    if let Some(path) = output_image_path {
         render(&scene, &mut canvas);
-        copy_canvas_to_texture(&canvas, &mut texture);
-        sdl_canvas.copy(
-            &texture,
-            None,
-            Some(Rect::new(0, 0, canvas_width, canvas_height)),
-        )?;
-        sdl_canvas.present();
+        save_canvas_to_file(&canvas, &path);
+    } else {
+        // Inialize the sdl
+        let sdl_context = sdl2::init()?;
+        let video_subsystem = sdl_context.video()?;
 
-        let now = Instant::now();
-        let diff = now - previous_now;
-        previous_now = now;
+        // Create the windows
+        let window = video_subsystem
+            .window("Raytracer", canvas_width, canvas_height)
+            .position_centered()
+            .opengl()
+            .build()
+            .map_err(|e| e.to_string())?;
 
-        let dt = diff.as_secs_f32();
-        println!("frame {}, dt {}s, {} fps", frame_count, dt, 1.0 / dt);
-        frame_count += 1;
+        // Create the SDL canvas
+        let mut sdl_canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+        // Create the output texture
+        let texture_creator = sdl_canvas.texture_creator();
+        let mut texture = texture_creator
+            .create_texture_streaming(PixelFormatEnum::RGB24, canvas_width, canvas_height)
+            .map_err(|e| e.to_string())?;
+
+        // Initialize the event loop
+        let mut event_pump = sdl_context.event_pump()?;
+
+        // Start the main loop
+        let mut previous_now = Instant::now();
+        let mut frame_count = 1;
+        'running: loop {
+            // Handle the events
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit { .. }
+                    | Event::KeyDown {
+                        keycode: Some(Keycode::Escape),
+                        ..
+                    } => break 'running,
+                    _ => {}
+                }
+            }
+
+            // Clear the canvas
+            sdl_canvas.set_draw_color(Color::RGB(255, 0, 255));
+            sdl_canvas.clear();
+
+            // Render the scene
+            render(&scene, &mut canvas);
+
+            // Update and draw the output texture with the content
+            // of our framebuffer
+            copy_canvas_to_texture(&canvas, &mut texture);
+            sdl_canvas.copy(
+                &texture,
+                None,
+                Some(Rect::new(0, 0, canvas_width, canvas_height)),
+            )?;
+
+            // Present the SDL canvas
+            sdl_canvas.present();
+
+            let now = Instant::now();
+            let diff = now - previous_now;
+            previous_now = now;
+
+            let dt = diff.as_secs_f32();
+            println!("frame {}, dt {}s, {} fps", frame_count, dt, 1.0 / dt);
+            frame_count += 1;
+        }
     }
 
     Ok(())
