fe8980b92ad0c4d13a43cf1921ec9f08ef8fd37c
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-09-06 22:48:34 +0200

    move camera to new file

---
+++ src/camera.rs
@@ -0,0 +1,45 @@
+use crate::math::Vector3;
+/**
+ * @brief A view port
+ */
+pub struct ViewPort {
+    pub width: f32,
+    pub height: f32,
+    pub distance: f32,
+}
+
+impl ViewPort {
+    pub fn new(width: f32, height: f32, distance: f32) -> Self {
+        Self {
+            width,
+            height,
+            distance,
+        }
+    }
+}
+
+impl Default for ViewPort {
+    fn default() -> Self {
+        Self::new(1.0, 1.0, 1.0)
+    }
+}
+
+pub struct Camera {
+    pub position: Vector3,
+    pub view_port: ViewPort,
+}
+
+impl Camera {
+    pub fn new(position: Vector3, view_port: ViewPort) -> Self {
+        Self {
+            position,
+            view_port,
+        }
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Self::new(Vector3::default(), ViewPort::default())
+    }
+}
--- src/scene.rs
+++ src/scene.rs
@@ -1,51 +1,6 @@
+use crate::camera::Camera;
 use crate::geometry::Sphere;
 use crate::light::Light;
-use crate::math::Vector3;
-
-/**
- * @brief A view port
- */
-pub struct ViewPort {
-    pub width: f32,
-    pub height: f32,
-    pub distance: f32,
-}
-
-impl ViewPort {
-    pub fn new(width: f32, height: f32, distance: f32) -> Self {
-        Self {
-            width,
-            height,
-            distance,
-        }
-    }
-}
-
-impl Default for ViewPort {
-    fn default() -> Self {
-        Self::new(1.0, 1.0, 1.0)
-    }
-}
-
-pub struct Camera {
-    pub position: Vector3,
-    pub view_port: ViewPort,
-}
-
-impl Camera {
-    pub fn new(position: Vector3, view_port: ViewPort) -> Self {
-        Self {
-            position,
-            view_port,
-        }
-    }
-}
-
-impl Default for Camera {
-    fn default() -> Self {
-        Self::new(Vector3::default(), ViewPort::default())
-    }
-}
 
 pub struct Scene {
     pub camera: Camera,
